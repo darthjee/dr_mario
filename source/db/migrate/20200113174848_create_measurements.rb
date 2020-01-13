6b55b269d28661e094ef7a71f96d52167572b96a
darthjee <darthjee@gmail.com>
2020-01-13 19:00:11 +0100

    Add measurement

---
+++ source/db/migrate/20200113174848_create_measurements.rb
@@ -0,0 +1,10 @@
+class CreateMeasurements < ActiveRecord::Migration[5.2]
+  def change
+    create_table :measurements do |t|
+      t.integer  :glicemy, limit: 1
+      t.date     :date
+      t.time     :time
+      t.timestamps
+    end
+  end
+end
