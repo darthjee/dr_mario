9f8e57f60dce8025bce68a6009eb7b35ca1de3bf
darthjee <darthjee@gmail.com>
2020-01-19 13:13:45 +0100

    Add home controller

---
+++ source/app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  include OnePageApplication
+
+  def show
+  end
+end
+
--- source/config/routes.rb
+++ source/config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/' => 'home#show', as: :home
+
   resources :users, only: [] do
     resources :measurements, only: %i[index show create]
   end
