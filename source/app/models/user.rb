3a70e8633c19db0058420db657ebd1983179c595
darthjee <darthjee@gmail.com>
2020-02-03 18:38:22 +0100

    Add user methods

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,2 +1,29 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates_presence_of :login, :email, :encrypted_password
+
+  def self.login(login:, password:)
+    User.find_by!(login: login).verify_password!(password)
+  rescue ActiveRecord::RecordNotFound
+    raise DrMario::Exception::LoginFailed
+  end
+
+  def password=(pass)
+    self.salt = SecureRandom.hex
+    self.encrypted_password = encrypt_password(pass)
+  end
+
+  def verify_password!(pass)
+    return self if encrypted_password == encrypt_password(pass)
+
+    raise DrMario::Exception::LoginFailed
+  end
+
+  private
+
+  def encrypt_password(pass)
+    plain = salt + pass + Settings.password_salt
+    Digest::SHA256.hexdigest(plain)
+  end
 end
