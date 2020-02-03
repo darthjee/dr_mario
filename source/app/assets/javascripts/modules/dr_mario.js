c1cd6ed41561e944df9661e08d6af0d6cd3f931f
darthjee <darthjee@gmail.com>
2020-02-03 15:17:43 +0100

    Extract routes file

--- source/app/assets/javascripts/modules/dr_mario.js
+++ source/app/assets/javascripts/modules/dr_mario.js
@@ -12,41 +12,4 @@
       'Content-Type': 'application/json;charset=utf-8'
     };
   }]);
-
-  module.config(['kantoProvider', function(provider) {
-    provider.defaultConfig = {
-      controller: 'Cyberhawk.Controller',
-      controllerAs: 'gnc',
-      templateBuilder: function(route, params) {
-        return route + '?ajax=true';
-      }
-    }
-
-    provider.configs = [{
-      routes: ['/'],
-      config: {
-        controller: 'Home.Controller',
-        controllerAs: 'hc'
-      }
-    }, {
-      routes: [
-        '/users/:user_id/measurements/new'
-      ],
-      config: {
-        controller: 'Measurement.FormController',
-        controllerAs: 'mc'
-      }
-    }, {
-      routes: [
-        '/users/:user_id/measurements'
-      ],
-      config: {
-        controller: 'Measurement.IndexController',
-        controllerAs: 'mc'
-      }
-    },{
-      routes: []
-    }];
-    provider.$get().bindRoutes();
-  }]);
 }(window.angular));
