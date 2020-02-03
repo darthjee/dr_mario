e6261f0bb122c2064525cf88b8ce805f4631cd20
darthjee <darthjee@gmail.com>
2020-02-03 18:16:52 +0100

    Add users

---
+++ source/db/migrate/20200203171437_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :login, null: false
+      t.string :email, null: false
+      t.string :encrypted_password, null: false
+      t.string :salt, null: false
+
+      t.timestamps
+
+      t.index :email, unique: true
+      t.index :login, unique: true
+    end
+  end
+end
--- source/db/schema.rb
+++ source/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_174848) do
+ActiveRecord::Schema.define(version: 2020_02_03_171437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2020_01_13_174848) do
     t.time "time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "salt", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["login"], name: "index_users_on_login", unique: true
   end
 
 end
