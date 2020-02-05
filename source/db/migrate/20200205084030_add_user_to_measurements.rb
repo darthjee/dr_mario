bf56d39151e9ac698b831dc556495bd33a2e92f0
darthjee <darthjee@gmail.com>
2020-02-05 09:46:29 +0100

    Add user_id to measurements

---
+++ source/db/migrate/20200205084030_add_user_to_measurements.rb
@@ -0,0 +1,13 @@
+class AddUserToMeasurements < ActiveRecord::Migration[5.2]
+  def up
+    change_table :measurements do |t|
+      t.integer :user_id, null: false, default: 1
+    end
+
+    change_column :measurements, :user_id, :integer, null: false, default: nil
+  end
+
+  def down
+    remove_column :measurements, :user_id
+  end
+end
