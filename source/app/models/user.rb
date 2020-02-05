d7e4400b9e7c01ee5f534757df71b9a44e66b281
darthjee <darthjee@gmail.com>
2020-02-05 18:09:16 +0100

    Add relations

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   validates_presence_of :login, :email, :encrypted_password
+  has_many :measurements
 
   def self.login(login:, password:)
     User.find_by!(login: login).verify_password!(password)
