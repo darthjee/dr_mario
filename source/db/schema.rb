5e642a03a5ddedadea9cb4dbc192da5336f448e0
darthjee <darthjee@gmail.com>
2020-02-26 19:35:27 +0100

    Update schema

--- source/db/schema.rb
+++ source/db/schema.rb
@@ -12,11 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2020_02_17_130445) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "measurements", force: :cascade do |t|
-    t.integer "glicemy", limit: 2, null: false
+  create_table "measurements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "glicemy", limit: 1, null: false
     t.date "date", null: false
     t.time "time", null: false
     t.datetime "created_at", null: false
@@ -24,14 +21,14 @@ ActiveRecord::Schema.define(version: 2020_02_17_130445) do
     t.integer "user_id", null: false
   end
 
-  create_table "sessions", force: :cascade do |t|
+  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "expiration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "login", null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
