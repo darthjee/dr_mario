43a682a8e1ee06cc43b17da7f52b2b94b9bed0cb
darthjee <darthjee@gmail.com>
2020-03-02 22:13:34 +0100

    Add function wrapper

--- source/app/assets/javascripts/utils/function_wrapper.js
+++ source/app/assets/javascripts/utils/function_wrapper.js
@@ -1,13 +1,28 @@
 (function(_) {
+  class FunctionWrapper {
+    constructor(object, method, wrapper) {
+      this.object = object
+      this.method = method
+      this.wrapper = wrapper
+      this.original = object[method]
+    }
+
+    build() {
+      var that = this;
+      
+      return function() {
+        var binded = _.bind(that.original, this)
+        var args = [binded].concat([].slice.call(arguments, 0));
+
+        return that.wrapper.apply(this, args);
+      };
+    }
+  }
+
   _.functionWrapper = function(object, method, wrapper) {
-    var original = object[method];
+    var wrapper = new FunctionWrapper(object, method, wrapper);
 
-    object[method] = function() {
-      var binded = _.bind(original, this)
-      var args = [binded].concat([].slice.call(arguments, 0));
-
-      return wrapper.apply(this, args);
-    };
+    object[method] = wrapper.build();
   };
 
   class T {
