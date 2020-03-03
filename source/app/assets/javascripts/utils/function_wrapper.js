cf52a574afbc586a0af519c228487efcc1db3263
darthjee <darthjee@gmail.com>
2020-03-03 16:59:45 +0100

    Merge wrapper

--- source/app/assets/javascripts/utils/function_wrapper.js
+++ source/app/assets/javascripts/utils/function_wrapper.js
@@ -7,39 +7,32 @@
       this.original = object[method];
     }
 
-    wrap() {
+    wrap(bindArguments) {
       var that = this;
       
       return function() {
-        var binded = _.bind(that.original, this)
+        var binded = that._bind(that.original, this, arguments, bindArguments);
         var args = [binded].concat([].slice.call(arguments, 0));
 
         return that.wrapper.apply(this, args);
       };
     }
 
-    bindedWrap() {
-      var that = this;
-      
-      return function() {
-        var binded = that._bind(that.original, this, arguments)
-        var args = [binded].concat([].slice.call(arguments, 0));
-
-        return that.wrapper.apply(this, args);
-      };
-    }
-
-    _bind(func, caller, args) {
-      return function() {
-        return func.apply(caller, args);
-      };
+    _bind(func, caller, args, bindArguments) {
+      if (bindArguments) {
+        return function() {
+          return func.apply(caller, args);
+        };
+      } else {
+        return _.bind(func, caller);
+      }
     }
   }
 
   _.functionWrapper = function(object, method, wrapper) {
     var wrapper = new FunctionWrapper(object, method, wrapper);
 
-    object[method] = wrapper.bindedWrap();
+    object[method] = wrapper.wrap();
   };
 
   class T {
@@ -52,10 +45,10 @@
 
   var t = new T();
 
-  _.functionWrapper(t, 'm', function(o) {
+  _.functionWrapper(t, 'm', function(o, a, b) {
     console.info(arguments);
     console.info('before');
-    var r = o();
+    var r = o(a, b, 3);
     console.info('after');
     return r;
   });
