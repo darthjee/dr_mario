d0f00707a430bb44d474d5510d873c3d49371cb4
darthjee <darthjee@gmail.com>
2020-03-09 16:16:35 +0100

    Add spec on field

--- source/spec/models/measurement_spec.rb
+++ source/spec/models/measurement_spec.rb
@@ -18,4 +18,14 @@ RSpec.describe Measurement, type: :model do
       expect(measurement).to validate_presence_of(:time)
     end
   end
+
+  describe "fields" do
+    context "with high glicemy" do
+      subject(:measurement) { build(:measurement, glicemy: 999) }
+
+      it "accepts high glicemy" do
+        expect { measurement.save! }.not_to raise_error
+      end
+    end
+  end
 end
