380fbe36ad95214ddb3bb2470fea7c9765b71c5c
darthjee <darthjee@gmail.com>
2020-03-17 14:18:27 +0100

    Add seed

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -3,3 +3,17 @@
 user = User.find_or_initialize_by(login: 'user', email: 'email@srv.com')
 user.password = 'pass'
 user.save
+
+4.times.each do |days|
+  day = days.days.ago
+  next if user.measurements.where(date: day).any?
+
+  3.times.each do |hour|
+    time = "%d:%02d:%02d" % [Random.rand(2) + 6 + 4 * hour, Random.rand(60), Random.rand(60)]
+
+    user.measurements.where(date: day).create(
+      time: time,
+      glicemy: Random.rand(100) + 50
+    )
+  end
+end
